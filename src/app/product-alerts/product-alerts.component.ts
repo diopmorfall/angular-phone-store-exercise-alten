81f934d764a6ea445822954356877291a13ad8f9
diopmorfall <morfall.papydiop99@gmail.com>
2022-02-08 12:10:45 +0100

    Clearer notes added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 // Input is needed to allow our component to receive data from the parent element
 // Output and EventEmitter are for handling events
+
 import { Product } from '../products';
 
 @Component({
@@ -8,13 +9,15 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
-// the class following this above will become a component and it'll also be exported
+// the following class below will become a component with the html above
 export class ProductAlertsComponent implements OnInit {
   //@Input() product!: Product;
   @Input() product: Product | undefined;
-  // input takes the data from the Product class
+  // takes the data from the Product class, which its value is stored in the parent
   @Output() notify = new EventEmitter();
-  // and here we can emit an event
+  // and here we kinda create this new event
+
+  // I'm always defining the properties that'll be used as attributes in the html template elements
 
   constructor() {}
 
